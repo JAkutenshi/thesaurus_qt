90fe941c6aacc47dbf06f2a9e8d4ddaf85aa397e
JAkutenshi <jakutenshi@gmail.com>
2020-07-28 23:37:57 +0300

    db_instance.* class : created

---
+++ src/db/db_instance.cpp
@@ -0,0 +1,6 @@
+#include "db_instance.h"
+
+DBInstance::DBInstance()
+{
+
+}
---
+++ src/db/db_instance.h
@@ -0,0 +1,11 @@
+#ifndef DB_INSTANCE_H
+#define DB_INSTANCE_H
+
+
+class DBInstance
+{
+public:
+    DBInstance();
+};
+
+#endif // DB_INSTANCE_H
