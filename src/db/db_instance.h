31cc7f1b271d5809c08a0181807c427b1a1ad896
JAkutenshi <jakutenshi@gmail.com>
2020-07-29 23:52:52 +0300

    db_instance.h : add interface

--- src/db/db_instance.h
+++ src/db/db_instance.h
@@ -1,11 +1,21 @@
 #ifndef DB_INSTANCE_H
 #define DB_INSTANCE_H
 
+#include <string>
 
 class DBInstance
 {
 public:
     DBInstance();
+
+    bool isDBexists();
+    bool isDBexists(std::string path);
+    DBInstance& connect();
+    DBInstance& connect(std::string path);
+    bool closeConection();
+
+
+
 };
 
 #endif // DB_INSTANCE_H
